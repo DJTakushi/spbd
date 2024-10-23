d9211665c909e523a155968341c742fac9e446f8
danny <dannytakushi@gmail.com>
2024-10-23 14:54:02 -0500

    loop faster!

--- sub/sub/main.cpp
+++ sub/sub/main.cpp
@@ -67,11 +67,8 @@ int main(int argc, char* argv[]) {
 
   int counter = 0;
   while(true) {
-    for (int j = 0; j < 50; j++) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        mosquitto_loop(mosq, 0, 1);
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    mosquitto_loop(mosq, 0, 1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
   }
 }
 
