a41ffa9d4eb6a2a36c52f5d6450241b1db36faf6
danny <dannytakushi@gmail.com>
2024-11-06 15:53:48 -0600

    metric removal from node/device birth

--- py_listen/listen.py
+++ py_listen/listen.py
@@ -69,50 +69,7 @@ def publishNodeBirth():
     # Create the node birth payload
     payload = sparkplug.getNodeBirthPayload()
 
-    # Set up the Node Controls
-    addMetric(payload, "Node Control/Next Server", None, MetricDataType.Boolean, False)
-    addMetric(payload, "Node Control/Rebirth", None, MetricDataType.Boolean, False)
-    addMetric(payload, "Node Control/Reboot", None, MetricDataType.Boolean, False)
-
-    # Add some regular node metrics
-    addMetric(payload, "Node Metric0", None, MetricDataType.String, "hello node")
-    addMetric(payload, "Node Metric1", None, MetricDataType.Boolean, True)
-    addNullMetric(payload, "Node Metric3", None, MetricDataType.Int32)
-
-    # Create a DataSet (012 - 345) two rows with Int8, Int16, and Int32 contents and headers Int8s, Int16s, Int32s and add it to the payload
-    columns = ["Int8s", "Int16s", "Int32s"]
-    types = [DataSetDataType.Int8, DataSetDataType.Int16, DataSetDataType.Int32]
-    dataset = initDatasetMetric(payload, "DataSet", None, columns, types)
-    row = dataset.rows.add()
-    element = row.elements.add();
-    element.int_value = 0
-    element = row.elements.add();
-    element.int_value = 1
-    element = row.elements.add();
-    element.int_value = 2
-    row = dataset.rows.add()
-    element = row.elements.add();
-    element.int_value = 3
-    element = row.elements.add();
-    element.int_value = 4
-    element = row.elements.add();
-    element.int_value = 5
-
-    # Add a metric with a custom property
-    metric = addMetric(payload, "Node Metric2", None, MetricDataType.Int16, 13)
-    metric.properties.keys.extend(["engUnit"])
-    propertyValue = metric.properties.values.add()
-    propertyValue.type = ParameterDataType.String
-    propertyValue.string_value = "MyCustomUnits"
-
-    # Create the UDT definition value which includes two UDT members and a single parameter and add it to the payload
-    template = initTemplateMetric(payload, "_types_/Custom_Motor", None, None)
-    templateParameter = template.parameters.add()
-    templateParameter.name = "Index"
-    templateParameter.type = ParameterDataType.String
-    templateParameter.string_value = "0"
-    addMetric(template, "RPMs", None, MetricDataType.Int32, 0)
-    addMetric(template, "AMPs", None, MetricDataType.Int32, 0)
+    # Add some device metrics (removed by takushi)
 
     # Publish the node birth certificate
     byteArray = bytearray(payload.SerializeToString())
@@ -124,21 +81,7 @@ def publishDeviceBirth():
     # Get the payload
     payload = sparkplug.getDeviceBirthPayload()
 
-    # Add some device metrics
-    addMetric(payload, "input/Device Metric0", None, MetricDataType.String, "hello device")
-    addMetric(payload, "input/Device Metric1", None, MetricDataType.Boolean, True)
-    addMetric(payload, "output/Device Metric2", None, MetricDataType.Int16, 16)
-    addMetric(payload, "output/Device Metric3", None, MetricDataType.Boolean, True)
-    addMetric(payload, "DateTime Metric", None, MetricDataType.DateTime, int(time.time() * 1000))
-
-    # Create the UDT definition value which includes two UDT members and a single parameter and add it to the payload
-    template = initTemplateMetric(payload, "My_Custom_Motor", None, "Custom_Motor")
-    templateParameter = template.parameters.add()
-    templateParameter.name = "Index"
-    templateParameter.type = ParameterDataType.String
-    templateParameter.string_value = "1"
-    addMetric(template, "RPMs", None, MetricDataType.Int32, 123)
-    addMetric(template, "AMPs", None, MetricDataType.Int32, 456)
+    # Add some device metrics (removed by takushi)
 
     # Publish the initial data with the Device BIRTH certificate
     totalByteArray = bytearray(payload.SerializeToString())
