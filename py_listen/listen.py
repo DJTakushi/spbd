9b8e761c91ea90b9ca72544f20bb57e1ef9de7d0
danny <dannytakushi@gmail.com>
2024-11-06 16:06:52 -0600

    indentation revision

--- py_listen/listen.py
+++ py_listen/listen.py
@@ -20,72 +20,72 @@ myUsername = "admin"
 myPassword = "changeme"
 
 def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected with result code "+str(rc))
-    else:
-        print("Failed to connect with result code "+str(rc))
-        sys.exit()
+  if rc == 0:
+    print("Connected with result code "+str(rc))
+  else:
+    print("Failed to connect with result code "+str(rc))
+    sys.exit()
 
-    global myGroupId
-    global myNodeName
+  global myGroupId
+  global myNodeName
 
-    print("starting subscriptions...")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("spBv1.0/" + myGroupId + "/NCMD/" + myNodeName + "/#")
-    print("subscribed 1...")
+  print("starting subscriptions...")
+  # Subscribing in on_connect() means that if we lose the connection and
+  # reconnect then subscriptions will be renewed.
+  client.subscribe("spBv1.0/" + myGroupId + "/NCMD/" + myNodeName + "/#")
+  print("subscribed 1...")
 
-    client.subscribe("spBv1.0/" + myGroupId + "/DCMD/" + myNodeName + "/#")
-    print("subscribed 2...")
+  client.subscribe("spBv1.0/" + myGroupId + "/DCMD/" + myNodeName + "/#")
+  print("subscribed 2...")
 
-    # client.subscribe("spBv1.0/Sparkplug B Devices/DDATA/danny")
-    # print("subscribed 3...")
+  # client.subscribe("spBv1.0/Sparkplug B Devices/DDATA/danny")
+  # print("subscribed 3...")
 
-    client.subscribe("spBv1.0/Sparkplug B Devices/DDATA/C Edge Node 1/Emulated Device")
-    print("subscribed 4...")
+  client.subscribe("spBv1.0/Sparkplug B Devices/DDATA/C Edge Node 1/Emulated Device")
+  print("subscribed 4...")
 
-    print("subscribed in on_connect()")
+  print("subscribed in on_connect()")
 
 def on_message(client, userdata, msg):
-    print("Message arrived: " + msg.topic)
-    # print("Message payload : " + msg.payload.decode("utf-8"))
-    tokens = msg.topic.split("/")
+  print("Message arrived: " + msg.topic)
+  # print("Message payload : " + msg.payload.decode("utf-8"))
+  tokens = msg.topic.split("/")
 
-    if tokens[0] == "spBv1.0" and tokens[1] == myGroupId and (tokens[2] == "DDATA") and tokens[3] == "C Edge Node 1":
-        inboundPayload = sparkplug_b_pb2.Payload()
-        inboundPayload.ParseFromString(msg.payload)
-        print(f"metrics size : {len(inboundPayload.metrics)}")
-        for metric in inboundPayload.metrics:
-            if metric.name == "engine_speed":
-                print(f"engine_speed : {metric.double_value}")
+  if tokens[0] == "spBv1.0" and tokens[1] == myGroupId and (tokens[2] == "DDATA") and tokens[3] == "C Edge Node 1":
+    inboundPayload = sparkplug_b_pb2.Payload()
+    inboundPayload.ParseFromString(msg.payload)
+    print(f"metrics size : {len(inboundPayload.metrics)}")
+    for metric in inboundPayload.metrics:
+      if metric.name == "engine_speed":
+        print(f"engine_speed : {metric.double_value}")
 
 def publishBirth(client):
-    publishNodeBirth(client)
-    publishDeviceBirth(client)
+  publishNodeBirth(client)
+  publishDeviceBirth(client)
 
 def publishNodeBirth(client):
-    print( "Publishing Node Birth")
+  print( "Publishing Node Birth")
 
-    # Create the node birth payload
-    payload = sparkplug.getNodeBirthPayload()
+  # Create the node birth payload
+  payload = sparkplug.getNodeBirthPayload()
 
-    # Add some device metrics (removed by takushi)
+  # Add some device metrics (removed by takushi)
 
-    # Publish the node birth certificate
-    byteArray = bytearray(payload.SerializeToString())
-    client.publish("spBv1.0/" + myGroupId + "/NBIRTH/" + myNodeName, byteArray, 0, False)
+  # Publish the node birth certificate
+  byteArray = bytearray(payload.SerializeToString())
+  client.publish("spBv1.0/" + myGroupId + "/NBIRTH/" + myNodeName, byteArray, 0, False)
 
 def publishDeviceBirth(client):
-    print( "Publishing Device Birth")
+  print( "Publishing Device Birth")
 
-    # Get the payload
-    payload = sparkplug.getDeviceBirthPayload()
+  # Get the payload
+  payload = sparkplug.getDeviceBirthPayload()
 
-    # Add some device metrics (removed by takushi)
+  # Add some device metrics (removed by takushi)
 
-    # Publish the initial data with the Device BIRTH certificate
-    totalByteArray = bytearray(payload.SerializeToString())
-    client.publish("spBv1.0/" + myGroupId + "/DBIRTH/" + myNodeName + "/" + myDeviceName, totalByteArray, 0, False)
+  # Publish the initial data with the Device BIRTH certificate
+  totalByteArray = bytearray(payload.SerializeToString())
+  client.publish("spBv1.0/" + myGroupId + "/DBIRTH/" + myNodeName + "/" + myDeviceName, totalByteArray, 0, False)
 
 def main():
   print("Starting main application")
@@ -110,27 +110,27 @@ def main():
   publishBirth(client)
 
   while True:
-      # Periodically publish some new data
-      payload = sparkplug.getDdataPayload()
+    # Periodically publish some new data
+    payload = sparkplug.getDdataPayload()
 
-      # Add some random data to the inputs
-      addMetric(payload, None, None, MetricDataType.String, ''.join(random.choice(string.ascii_lowercase) for i in range(12)))
+    # Add some random data to the inputs
+    addMetric(payload, None, None, MetricDataType.String, ''.join(random.choice(string.ascii_lowercase) for i in range(12)))
 
-      # Note this data we're setting to STALE via the propertyset as an example
-      metric = addMetric(payload, None, None, MetricDataType.Boolean, random.choice([True, False]))
-      metric.properties.keys.extend(["Quality"])
-      propertyValue = metric.properties.values.add()
-      propertyValue.type = ParameterDataType.Int32
-      propertyValue.int_value = 500
+    # Note this data we're setting to STALE via the propertyset as an example
+    metric = addMetric(payload, None, None, MetricDataType.Boolean, random.choice([True, False]))
+    metric.properties.keys.extend(["Quality"])
+    propertyValue = metric.properties.values.add()
+    propertyValue.type = ParameterDataType.Int32
+    propertyValue.int_value = 500
 
-      # Publish a message data
-      byteArray = bytearray(payload.SerializeToString())
-      client.publish("spBv1.0/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, byteArray, 0, False)
+    # Publish a message data
+    byteArray = bytearray(payload.SerializeToString())
+    client.publish("spBv1.0/" + myGroupId + "/DDATA/" + myNodeName + "/" + myDeviceName, byteArray, 0, False)
 
-      # Sit and wait for inbound or outbound events
-      for _ in range(5):
-          time.sleep(.1)
-          client.loop()
+    # Sit and wait for inbound or outbound events
+    for _ in range(5):
+      time.sleep(.1)
+      client.loop()
 
 if __name__=="__main__":
   main()
