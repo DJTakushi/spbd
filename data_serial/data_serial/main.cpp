ee03cdb626d40c670146d1e1abe676aba45ba7e2
danny <dannytakushi@gmail.com>
2024-11-08 15:03:48 -0600

    json generation; get_serial_port_name(); formatting++

--- data_serial/data_serial/main.cpp
+++ data_serial/data_serial/main.cpp
@@ -1,9 +1,15 @@
-#include <boost/asio.hpp>
+#include "boost/asio.hpp"
 #include "boost/asio/serial_port.hpp"
+#include "boost/algorithm/string.hpp"
+
 
 #include <csignal>
 #include <iostream>
 #include <thread>
+
+#include "nlohmann/json.hpp"
+
+#define DEFAULT_SERIAL_PORT_NAME "/dev/pts/12"
 
 std::string serial_tag = "--serial=";
 boost::asio::io_service m_ioService;
@@ -22,21 +28,40 @@ void sig_int_handler(int signum) {
   exit_application(1);
 }
 
+std::string get_serial_port_name(int argc, char* argv[]){
+  std::string out = DEFAULT_SERIAL_PORT_NAME;
+  for(size_t i = 0; i <= (argc-1); i++) {
+    std::string str(argv[i]);
+    if(str.find(serial_tag) != std::string::npos){
+      out = str.erase(0,serial_tag.length());
+    }
+  }
+  return out;
+}
+
+nlohmann::ordered_json gen_metrics_from_serial(std::string str){
+  nlohmann::ordered_json j;
+  std::vector<std::string> parts;
+  boost::split(parts,str,boost::is_any_of(","));
+
+  size_t counter = 0;
+  for(auto part : parts) {
+    std::string metric_name = "metric"+std::to_string(counter);
+    boost::trim(part);
+    j[metric_name] = part;
+    counter++;
+  }
+
+  return j;
+}
+
 int main(int argc, char* argv[]) {
   signal(SIGINT, sig_int_handler); // registar for ctrl-c
   signal(SIGTERM, exit_application); // terminate from Docker STOPSIGNAL
 
-  std::string serial_port_name = "/dev/pts/12";
-
-  for(size_t i = 0; i <= (argc-1); i++) {
-    std::string str(argv[i]);
-    if(str.find(serial_tag) != std::string::npos){
-      serial_port_name = str.erase(0,serial_tag.length());
-      std::cout << "serial_port_name : " << serial_port_name << std::endl;
-    }
-  }
-
+  std::string serial_port_name = get_serial_port_name(argc,argv);
   serial_port.open(serial_port_name);
+  std::cout << "opened serial port " << serial_port_name << std::endl;
 
   while(true){
     std::string str = "";
@@ -47,7 +72,8 @@ int main(int argc, char* argv[]) {
       str += c;
     }
 
-    std::cout << "str : " << str;
+    nlohmann::ordered_json j = gen_metrics_from_serial(str);
+    std::cout << j.dump() << std::endl;
   }
 
   std::cout << "...exiting main.  goodbye." << std::endl;
