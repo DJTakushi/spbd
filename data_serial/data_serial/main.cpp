b144c8a5cd38c718a86ef52ec2301fa0ced3b9c3
danny <dannytakushi@gmail.com>
2024-11-08 10:06:45 -0600

    serial input with serial_tag

--- data_serial/data_serial/main.cpp
+++ data_serial/data_serial/main.cpp
@@ -7,6 +7,7 @@
 LibSerial::SerialPort serial_port;
 // LibSerial::SerialStream my_serial_stream( "/dev/ttyUSB0" ) ;
 
+std::string serial_tag = "--serial=";
 
 void exit_application(int signum) {
   std::cout  << "exiting sub application..."<<std::endl;
@@ -28,7 +29,17 @@ int main(int argc, char* argv[]) {
   signal(SIGINT, sig_int_handler); // registar for ctrl-c
   signal(SIGTERM, exit_application); // terminate from Docker STOPSIGNAL
 
-  serial_port.Open("/dev/pts/15");
+  std::string serial_port_name = "/dev/pts/12";
+
+  for(size_t i = 0; i <= (argc-1); i++) {
+    std::string str(argv[i]);
+    if(str.find(serial_tag) != std::string::npos){
+      serial_port_name = str.erase(0,serial_tag.length());
+      std::cout << "serial_port_name : " << serial_port_name << std::endl;
+    }
+  }
+
+  serial_port.Open(serial_port_name);
   // my_serial_stream.Open("/dev/pts/15");
 
   while(true){
