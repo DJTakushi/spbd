80e276c556704cd65cb3cfb17062cbc602c44ddd
danny <dannytakushi@gmail.com>
2024-11-12 17:48:30 -0600

    display connection being created

--- data_serial/connection/src/connection_factory.cpp
+++ data_serial/connection/src/connection_factory.cpp
@@ -1,13 +1,16 @@
+#include <iostream>
 #include "connection_factory.h"
 
 std::shared_ptr<connection_i> connection_factory::create(connection_type type) {
   std::shared_ptr<connection_i> out;
   switch(type){
     case kAzureIot:
+      std::cout << "creating azure iot connection..."<<std::endl;
       out = std::make_shared<connection_azure_routes>(100);
       break;
     case kMqtt:
     default:
+      std::cout << "creating mqtt connection..."<<std::endl;
       out = std::make_shared<connection_mqtt>("localhost",1883,100);
   }
   return out;
