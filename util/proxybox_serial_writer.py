b7a9243fb38df818e22dc0502cadcc9919bfb856
danny <dannytakushi@gmail.com>
2024-11-12 17:53:34 -0600

    dynamically write first argument 0-255

--- util/proxybox_serial_writer.py
+++ util/proxybox_serial_writer.py
@@ -1,13 +1,18 @@
 import time
 import serial #https://pythonhosted.org/pyserial/pyserial.html#overview
 
+counter  = 0
 while True:
-  mydata = b"0,1,2,3,4,5,6,7,8,9,10,11\n"
+  mydata = f"{counter},1,2,3,4,5,6,7,8,9,10,11\n"
   ser = serial.Serial('/dev/ttymxc0',
                       baudrate=115200,
                       bytesize=8,
                       parity='N',
                       stopbits=1,
                       timeout=0.5)
-  ser.write(mydata)
+  ser.write(bytes(mydata,'utf-8'))
+  if counter > 255:
+    counter = 0
+  else:
+    counter +=1
   time.sleep(1)
