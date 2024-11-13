3d9d3fe62f358ba1bf5f1a0b64385f748a71b34f
danny <dannytakushi@gmail.com>
2024-11-13 14:17:15 -0600

    write 14 vals

--- util/proxybox_serial_writer.py
+++ util/proxybox_serial_writer.py
@@ -3,7 +3,7 @@ import serial #https://pythonhosted.org/pyserial/pyserial.html#overview
 
 counter  = 0
 while True:
-  mydata = f"{counter},1,2,3,4,5,6,7,8,9,10,11\n"
+  mydata = f"{counter},1,2,3,4,5,6,7,8,9,10,11,12,13\n"
   ser = serial.Serial('/dev/ttymxc0',
                       baudrate=115200,
                       bytesize=8,
