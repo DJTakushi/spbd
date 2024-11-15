bda1964df5418929dbe4ded66623951490128e68
danny <dannytakushi@gmail.com>
2024-11-15 14:16:23 -0600

    add timestamp

--- een/een/main.cpp
+++ een/een/main.cpp
@@ -12,10 +12,19 @@ void sig_int_handler(int signum) {
   exit_application(1);
 }
 
+static std::string time_rfc_3339() {
+  /** https://en.cppreference.com/w/cpp/chrono/c/strftim */
+  std::time_t time = std::time({});
+  char timeString[std::size("yyyy-mm-ddThh:mm:ssZ")];
+  std::strftime(std::data(timeString), std::size(timeString),
+                "%FT%TZ", std::gmtime(&time));
+  return timeString;
+}
+
 int main(int argc, char* argv[]) {
   std::shared_ptr<een> een_ = std::make_shared<een>("");
 
-  std::cout << "starting service routine.."<<std::endl;
+  std::cout << time_rfc_3339() << " : starting service routine.."<<std::endl;
   while(een_->is_stable()){
     een_->service_mqtt();
     een_->service_iot_hub();
