c51a4ac3c418191ccd666909a498737782bc8330
danny <dannytakushi@gmail.com>
2024-12-04 16:59:01 -0600

    mqtt_listen improvements

--- util/mqtt_listen/mqtt_listen.py
+++ util/mqtt_listen/mqtt_listen.py
@@ -1,5 +1,6 @@
 # sudo docker build . -t mqtt_listen && sudo docker run pylisten
 import datetime
+import os
 import sys
 import time
 
@@ -12,13 +13,12 @@ mqtt_keepalive = 60
 myUsername = "admin"
 myPassword = "changeme"
 
-subscriptions = [
-  f"data_serial"
-]
+subscriptions = []
+default_subscription = "data_serial_output"
 
 def on_connect(client, userdata, flags, rc):
   if rc == 0:
-    print("Connected with result code "+str(rc))
+    print(f"Connected to {serverUrl} with result code "+str(rc))
   else:
     print("Failed to connect with result code "+str(rc))
     sys.exit()
@@ -40,9 +40,26 @@ def main():
   client.username_pw_set(myUsername, myPassword)
   client.connect(serverUrl, mqtt_port, mqtt_keepalive)
 
-  while True: # Sit and wait for inbound or outbound events
-    time.sleep(.1)
-    client.loop()
+  try:
+    while True: # Sit and wait for inbound or outbound events
+      time.sleep(.1)
+      client.loop()
+  except KeyboardInterrupt:
+    print('exiting...')
+    try:
+      sys.exit(0)
+    except SystemExit:
+      os._exit(0)
 
 if __name__=="__main__":
+  for i in sys.argv[1:]:
+    if i[0:2] == "-a":
+      serverUrl = i[2:]
+    elif i[0:2] == "-s":
+      subscriptions.append(i[2:])
+    else:
+      print("unknown argument: \""+i+"\"")
+  if len(subscriptions) == 0:
+    print(f"applying default subscription : {default_subscription}")
+    subscriptions.append(default_subscription)
   main()
